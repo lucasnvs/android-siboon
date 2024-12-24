5b95d3a9374ba718b4ee38deea8dcaeb33ebf97a
Lucas <lucasli.nev@hotmail.com>
2024-12-24 14:28:35 -0300

    feat: Cart total

--- app/src/main/java/com/lucasnvs/siboon/ui/cart/CartFragment.java
+++ app/src/main/java/com/lucasnvs/siboon/ui/cart/CartFragment.java
@@ -16,9 +16,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.lucasnvs.siboon.data.repository.ProductRepository;
 import com.lucasnvs.siboon.databinding.FragmentCartBinding;
+import com.lucasnvs.siboon.model.Cart;
 import com.lucasnvs.siboon.utils.SpacesItemDecoration;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CartFragment extends Fragment {
 
@@ -50,6 +52,10 @@ public class CartFragment extends Fragment {
         CartAdapter cartAdapter = new CartAdapter(getContext(), new ArrayList<>());
         binding.rvCart.setAdapter(cartAdapter);
 
+        binding.btnFinishOrder.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "Pedido finalizado com êxito.", Toast.LENGTH_SHORT).show();
+        });
+
         // The Decrease and the Increase buttons cause a bug: when clicked, the fragment reloads all the items in the cart.
         cartViewModel.cart.observe(getViewLifecycleOwner(), cartList -> {
             Boolean isEmpty = cartList != null && cartList.isEmpty();
@@ -58,6 +64,8 @@ public class CartFragment extends Fragment {
             binding.layoutCart.setVisibility(Boolean.TRUE.equals(isEmpty) ? View.GONE : View.VISIBLE);
 
             if(!isEmpty) {
+                assert cartList != null;
+                updateUiTotal(cartList);
                 cartAdapter.setProducts(cartList);
                 binding.rvCart.setAdapter(cartAdapter);
             }
@@ -73,6 +81,21 @@ public class CartFragment extends Fragment {
 
         return root;
     }
+
+    private void updateUiTotal(List<Cart> cart) {
+        if (cart == null || cart.isEmpty()) {
+            binding.tvTotalAmount.setText("R$ 0,00");
+            return;
+        }
+
+        double total = cart.stream()
+                .filter(item -> item.getProduct() != null)
+                .mapToDouble(item -> item.getProduct().getPrice() * item.getQuantity())
+                .sum();
+
+        binding.tvTotalAmount.setText(String.format("R$ %.2f", total));
+    }
+
 
     private void setToolbar() {
         Toolbar toolbar = binding.toolbar;
